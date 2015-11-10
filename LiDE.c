25aa505b04c6480a88e7a444c981d9c74243fcfd
Alain Martel <dev@martel.srsly.ca>
2015-11-10 05:00:12 -0500

    Update LiDE.c

--- LiDE.c
+++ LiDE.c
@@ -8,7 +8,7 @@
 #include "LiDE.h"
 
 void print_help(void){
-	printf("LiDE - v0.0.1.3 \n \
+	printf("LiDE - v0.0.1.4 \n \
 			-h: Prints this help.\n \
 			-v: Be verbose.\n \
 			-p: Specify a port to listen on. Default 31337.\n");
