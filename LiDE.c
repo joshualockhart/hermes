5f55d63c2e1bdb3c9a6c2bc48a0b48b261d2b21a
Alain Martel <dev@martel.srsly.ca>
2015-11-10 17:31:49 -0500

    Update LiDE.c
    -Fixed MD5 function error displayed if no file available.

--- LiDE.c
+++ LiDE.c
@@ -1,14 +1,37 @@
 #include <stdio.h>        // C library to perform Input/Output operations
-#include <unistd.h>       //
+#include <unistd.h>       // C standard symbolic constants and types
 #include <string.h>       // C Strings
 #include <stdlib.h>       // C Standard General Utilities Library
-#include <arpa/inet.h>    //
-#include <sys/socket.h>   //
-#include <sys/types.h>    //
-#include "LiDE.h"         //
+#include <arpa/inet.h>    // C definitions for internet operations
+#include <sys/socket.h>   // C Internet Protocol family
+#include <sys/types.h>    // C data types
+#include "LiDE.h"         // 
+//====Start-MD5-Support====
+#include <sys/stat.h>     // C data returned by the stat() function    --|
+#include <sys/mman.h>     // C memory management declarations            |== Support MD5 Functions
+#include <fcntl.h>        // C file control options                      |
+#include <openssl/md5.h>  //                                           --|
+
+unsigned char result[MD5_DIGEST_LENGTH];
+
+// Print the MD5 sum as hex-digits.
+void print_md5_sum(unsigned char* md) {
+    int i;
+    for(i=0; i <MD5_DIGEST_LENGTH; i++) {
+            printf("%02x",md[i]);
+    }
+}
+
+// Get the size of the file by its file descriptor
+unsigned long get_size_by_fd(int fd) {
+    struct stat statbuf;
+    if(fstat(fd, &statbuf) < 0) exit(-1);
+    return statbuf.st_size;
+}
+//====End-MD5-Support====
 
 void print_help(void){
-	printf("\x1B[33mLiDE (\033[1mLi\033[22mnux \033[1mD\033[22misk \033[1mE\033[22mxtractor) - v0.0.1.5\033[0m\n\n\
+	printf("\x1B[33mLiDE (\033[1mLi\033[22mnux \033[1mD\033[22misk \033[1mE\033[22mxtractor) - v0.0.2.1\033[0m\n\n\
 LiDE is a very small fileserver.\n\
 To host a file, do\n\n\
 $ \x1B[32m./lide <filename>\033[0m              Will use default port 31337\n\
@@ -27,6 +50,12 @@ int main(int argc, char *argv[])
 	FILE *filename_fd;
 	char *filename_to_serve;
 	int listen_port = htons(31337);
+
+	//====Start-MD5-Support====
+	int file_descript;
+	unsigned long file_size;
+	char* file_buffer;
+	//====End-MD5-Support====
 
 	if (argc < 2)
 		print_help();
@@ -76,8 +105,19 @@ int main(int argc, char *argv[])
 		strncpy(filename_to_serve, argv[argc-1], strlen(argv[argc-1]));
 	}
 
-
 	if ((filename_fd = fopen(filename_to_serve, "r")) != NULL){
+	//====Start-MD5-Functions==== 
+	file_descript = open(filename_to_serve, O_RDONLY);
+	file_size = get_size_by_fd(file_descript);
+    
+	file_buffer = mmap(0, file_size, PROT_READ, MAP_SHARED, file_descript, 0);
+	MD5((unsigned char*) file_buffer, file_size, result);
+	munmap(file_buffer, file_size); 
+
+	printf("\x1B[32m");
+	print_md5_sum(result);
+	printf("  \t%s\t%lu\040bytes\033[0m\n", argv[argc-1], file_size);
+	//====End-MD5-Functions====
 		listen_for_inbound_requests(listen_port, filename_fd);
 		printf("%d\n", listen_port);
 	}
